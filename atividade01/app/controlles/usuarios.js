e477e5ee079b064890bd01bd77c7012a16cf7bc1
Eduardo <eduardooffside@gmail.com>
2021-05-22 16:41:25 -0300

    func(no-name)

--- atividade01/app/controlles/usuarios.js
+++ atividade01/app/controlles/usuarios.js
@@ -1,33 +1,39 @@
 /*
-npm install nodemon -g
+npm install nodemon -g (start server)
+npm install body body-parser --save (body->POST)
 */
 
 let usuarios = [
-    {id: 1, nome: "Victor", email: "victor.aefarias@gmail.com", senha: "123"},
-    {id: 5, nome: "Joao", email: "joao@gmail.com", senha: "456"},
-    {id: 10, nome: "Eduardo", email: "eduardosilva@gmail.com", senha: "789"}
+    { id: 1, nome: "Victor", email: "victor.aefarias@gmail.com", senha: "123" },
+    { id: 5, nome: "Joao", email: "joao@gmail.com", senha: "456" },
+    { id: 10, nome: "Eduardo", email: "eduardosilva@gmail.com", senha: "789" }
 ]
 
-module.exports.listarUsuarios = function(req, res){
+module.exports.listarUsuarios = function (req, res) {
     let usuariosReturn = usuarios
-
-    if(req.query.nome) {
+    let cont = 0
+    if (req.query.nome) {
         let name = req.query.nome
-        usuariosReturn = usuariosReturn.filter(function(user){
-            return user.nome == name;
+        usuariosReturn = usuariosReturn.filter(function (user) {
+            if (user.nome == name) {
+                cont++
+                return user.nome == name;
+            }
         })
+        if (cont == 0)
+            res.status(404)
     }
     res.json(usuariosReturn)
 }
 
-module.exports.listarUsuariosPorId = function(req, res){
+module.exports.listarUsuariosPorId = function (req, res) {
     let id_ = req.params.id
-    let usuario = usuarios.find(function(user){
+    let usuario = usuarios.find(function (user) {
         return user.id == id_
     })
 
-    if(usuario) 
+    if (usuario)
         res.json(usuario)
-    else 
-        res.status(404).json({mensagem: "Usuario nao encontrado"})
+    else
+        res.status(404).json({ mensagem: "Usuario nao encontrado" })
 }
