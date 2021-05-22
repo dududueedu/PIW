a7f2f0d1a95a811c9436b800cbc10a5cd333e8dc
Eduardo <eduardooffside@gmail.com>
2021-05-22 00:43:14 -0300

    code-clean query-parameter

--- atividade01/app/controlles/usuarios.js
+++ atividade01/app/controlles/usuarios.js
@@ -4,22 +4,30 @@ npm install nodemon -g
 
 let usuarios = [
     {id: 1, nome: "Victor", email: "victor.aefarias@gmail.com", senha: "123"},
-    {id: 5, nome: "João", email: "joao@gmail.com", senha: "456"},
+    {id: 5, nome: "Joao", email: "joao@gmail.com", senha: "456"},
     {id: 10, nome: "Eduardo", email: "eduardosilva@gmail.com", senha: "789"}
 ]
 
 module.exports.listarUsuarios = function(req, res){
-    res.json(usuarios)
+    let usuariosReturn = usuarios
+
+    if(req.query.nome) {
+        let name = req.query.nome
+        usuariosReturn = usuariosReturn.filter(function(user){
+            return user.nome == name;
+        })
+    }
+    res.json(usuariosReturn)
 }
 
 module.exports.listarUsuariosPorId = function(req, res){
-    let id_usuario = req.params.id
-    let usuario = usuarios.find(function(usuario){
-        return usuario.id == id_usuario
+    let id_ = req.params.id
+    let usuario = usuarios.find(function(user){
+        return user.id == id_
     })
 
     if(usuario) 
         res.json(usuario)
     else 
-        res.json({mensagem: "Usuario nao encontrado"}).status(404) //status code não alterado.
+        res.status(404).json({mensagem: "Usuario nao encontrado"})
 }
--- atividade01/config/express.js
+++ atividade01/config/express.js
@@ -3,11 +3,9 @@ const express = require('express')
 const routerUsuarios = require('../app/routes/usuarios')
 //const bodyParser = require("body-parser")
 
-// Exportando módulo (Padrão CommonJS)
 module.exports = function () {
     let app = express()
 
-    // Definindo variável de aplicação
     app.set("port", 8393)
     //app.use(bodyParser.json()); x
     //app.use(bodyParser.urlencoded({ extended: false })); x
