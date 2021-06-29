b6455f95813da58d5aaf60e85d116ea8b25e82c2
Eduardo <eduardooffside@gmail.com>
2021-06-29 15:43:30 -0300

    token User

--- atividade03/app/controlles/authUsers.js
+++ atividade03/app/controlles/authUsers.js
@@ -1,15 +1,17 @@
 const Usuario = require("../models/UsuarioM")
 const bcrypt = require("Bcrypt")
+const jwt = require("jsonwebtoken")
 
-module.exports.logar = function (req, res){
-    Usuario.findOne({email: req.body.email})
-        .then(function (user){
-            if(bcrypt.compareSync(req.body.senha, user.senha)){
-                res.status(200).send("credenciais ok!")
+module.exports.logar = function (req, res) {
+    Usuario.findOne({ email: req.body.email })
+        .then(function (user) {
+            if (bcrypt.compareSync(req.body.senha, user.senha)) {
+                let token = jwt.sign({ id: user.id }, "senhaSecreta")
+                res.status(200).json({ token: token })
             }
             res.status(401).send("credenciais inválidas!")
         })
-        .catch(function (){
+        .catch(function () {
             res.status(401).send("credenciais erradas!")
         })
 }
