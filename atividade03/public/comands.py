d9ad2a23e02bdf548111fa44573c24955ed0e158
Eduardo <eduardooffside@gmail.com>
2021-07-02 18:28:24 -0300

    update comands

--- atividade03/public/comands.py
+++ atividade03/public/comands.py
@@ -1,3 +1,4 @@
 # npm install --save bcrypt
 # npm install --save jsonwebtoken
 # logar, checar, inserir, excluir
+# db.posts.remove( {"_id": ObjectId("60df6e64d83fe730142c7090")}) //excluir um item por ID
