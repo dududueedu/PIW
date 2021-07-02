5a2a6b1a28e2343b08d4533215280781b1e541f5
Eduardo <eduardooffside@gmail.com>
2021-07-02 18:20:57 -0300

    upgrade excluirUsuario - check (token)

--- atividade03/app/controlles/usuarios.js
+++ atividade03/app/controlles/usuarios.js
@@ -6,6 +6,7 @@ const ViewPost = require("../views/PostV")
 const View = require("../views/UsuarioV") 
 //senhas hasheadas
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken") 
 
 module.exports.listarUsuarios = function (req, res) {
     let promise = Usuario.find().exec()
@@ -55,18 +56,26 @@ module.exports.inserirUsuario = function(req, res){
 
 // Controller to DELETE-id
 module.exports.excluirUsuario = function(req, res){
-    let id_ = req.params.id
-    let promise = Usuario.findByIdAndDelete(id_)
 
-    promise.then(
-        function (usuario) {
-            res.status(200).json(View.render(usuario))
-        }
-    ).catch(
-        function (error) {
-            res.status(400).json({ mensagem: "Não foi possível remover usuário.", error: error })
-        }
-    )
+    let idUser = req.params.id
+    let token = req.headers.token
+    let paypload = jwt.decode(token)
+    let id_userLogado = paypload.id
+
+    if(idUser == id_userLogado){
+        let promise = Usuario.findOneAndDelete(id_userLogado)
+        promise.then(
+            function (usuario) {
+                res.status(200).json(View.render(usuario))
+            }
+        ).catch(
+            function (error) {
+                res.status(400).json({ mensagem: "Não foi possível remover usuário, erro interno.", error: error })
+            }
+        )
+    }else{
+        res.status(401).json({ mensagem: "Não foi possível remover usuário. (token =!)"})
+    }
 }
 
 module.exports.buscarPostsUsuarioPorID = function(req, res){
