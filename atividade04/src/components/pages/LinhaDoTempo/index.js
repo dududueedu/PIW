9aded1c95280570a9cd65f42f9e12ea79034a2ca
Eduardo <eduardooffside@gmail.com>
2021-07-14 23:21:28 -0300

    create LinhaDoTempo

---
+++ atividade04/src/components/pages/LinhaDoTempo/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+function LinhaDoTempo() {
+    return (
+        <div className="linha-do-tempo">
+            <img src="https://picsum.photos/200/200" alt="img"/>
+        </div>
+    )
+}
+export default LinhaDoTempo
--- atividade04/src/components/pages/PaginaFeed/index.js
+++ atividade04/src/components/pages/PaginaFeed/index.js
@@ -1,10 +1,12 @@
 import Navegador from "../../commom/Navegador"
+import LinhaDoTempo from '../../pages/LinhaDoTempo'
 import './index.css'
 
 function PaginaFeed() {
     return (
         <div className="container">
             <Navegador />
+            <LinhaDoTempo />
         </div>
     )
 }
