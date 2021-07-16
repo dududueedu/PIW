1675a57c95081583788da30449592b11e5f93e8b
dududueedu <eduardooffside@gmail.com>
2021-07-16 01:17:20 -0300

    up component/css - Post

--- atividade04/src/components/pages/Post/index.js
+++ atividade04/src/components/pages/Post/index.js
@@ -1,11 +1,23 @@
 import './index.css'
+import { useState } from 'react'
 
-function Post({nome, msg, qtd_likes}) {
+function Post({ nome, msg, qtd_likes }) {
+
+    const [qtd_li, setQTD_likes] = useState(qtd_likes)
+    console.log(qtd_li)
+
+    function addLikes(){
+        setQTD_likes(qtd_li + 1)
+    }
+
     return (
         <div className="post">
-            <h3 className="nomePessoa">{nome}</h3>
+            <h3 className="nomeUsuario">{nome}</h3>
             <span className="mensagem">"{msg}"</span>
-            <span className="qtdLikes">{qtd_likes} likes</span>
+            <div className="likeEbotao">
+                <span className="qtdLikes">{qtd_li} likes</span>
+                <button onClick={addLikes} className="botaoDoPost">Curtir</button>
+            </div>
         </div>
     )
 }
