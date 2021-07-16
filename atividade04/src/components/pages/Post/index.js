a508d10d913a5ee12617f81ae64e9599fe703fda
dududueedu <eduardooffside@gmail.com>
2021-07-16 00:06:28 -0300

    up component Post

--- atividade04/src/components/pages/Post/index.js
+++ atividade04/src/components/pages/Post/index.js
@@ -4,8 +4,8 @@ function Post({nome, msg, qtd_likes}) {
     return (
         <div className="post">
             <h3 className="nomePessoa">{nome}</h3>
-            <span className="mensagem">{msg}</span>
-            <span className="qtdLikes">{qtd_likes}</span>
+            <span className="mensagem">"{msg}"</span>
+            <span className="qtdLikes">{qtd_likes} likes</span>
         </div>
     )
 }
