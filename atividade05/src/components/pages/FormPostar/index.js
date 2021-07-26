c828cf27fe74f119bc6c6bf51464195903d0ada0
dududueedu <eduardooffside@gmail.com>
2021-07-26 17:14:16 -0300

    activity continuation

---
+++ atividade05/src/components/pages/FormPostar/index.js
@@ -0,0 +1,55 @@
+import { Form, Input, Button } from 'antd';
+import './index.css'
+
+function FormPostar (){
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Por favor, escreva sua mensagem.',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormPostar
--- atividade05/src/components/pages/PaginaPostar/index.js
+++ atividade05/src/components/pages/PaginaPostar/index.js
@@ -1,4 +1,5 @@
 import Navegador from '../../commom/Navegador'
+import FormPostar from '../FormPostar'
 import './index.css'
 
 function PaginaPostar() {
@@ -6,18 +7,7 @@ function PaginaPostar() {
         <div className="container">
             <Navegador />
             <div>
-                <h3>componente FormPostar</h3>
-                <p>a. Deve conter um campo para digitar a mensagem</p>
-                <span>
-                    b.
-                    Deve conter o botão de
-                    submeter.
-                    Ao clicar nesse botão,</span><p>
-                    deve redirecionar
-                    para PaginaFeed. Não precisa
-                    implementar a <p>parte de adicionar comentário nesse
-                    momento.</p>
-                </p>
+                <FormPostar />
             </div>
         </div>
     )
