e3ea51d30694d7d06adac9a1a0ba17b1361c620f
dududueedu <eduardooffside@gmail.com>
2021-08-09 17:35:51 -0300

    add routes (cadastro-login)

--- atividade06/src/App.js
+++ atividade06/src/App.js
@@ -2,6 +2,8 @@ import { Router, Route } from 'react-router-dom'
 import PaginaFeed from './components/pages/PaginaFeed'
 import PaginaPostar from './components/pages/PaginaPostar'
 import history from './components/History'
+import PaginaCadastro from './components/pages/PaginaCadastro';
+import PaginaLogin from './components/pages/PaginaLogin';
 
 function App() {
   return (
@@ -12,6 +14,14 @@ function App() {
 
       <Route path="/pagpostar">
         <PaginaPostar />
+      </Route>
+
+      <Route path="/cadastro">
+        <PaginaCadastro />
+      </Route>
+
+      <Route path="/login">
+        <PaginaLogin />
       </Route>
     </Router>
   );
