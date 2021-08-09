f121d169adc3b1c5038db427cb6e0361533133f6
dududueedu <eduardooffside@gmail.com>
2021-08-09 17:29:10 -0300

    create pag(cadastro-login)

---
+++ atividade06/src/components/pages/PaginaCadastro/index.js
@@ -0,0 +1,11 @@
+
+
+function PaginaCadastro(){
+    return(
+        <div>
+            <span>pagina de cadastro</span>
+        </div>
+    )
+}
+
+export default PaginaCadastro
---
+++ atividade06/src/components/pages/PaginaLogin/index.js
@@ -0,0 +1,11 @@
+
+
+function PaginaLogin(){
+    return(
+        <div>
+            <span>pagina de login</span>
+        </div>
+    )
+}
+
+export default PaginaLogin
