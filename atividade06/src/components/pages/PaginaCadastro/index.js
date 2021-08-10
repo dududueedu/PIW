03b85cec9610f9b4711f92b64894ea84708144e2
dududueedu <eduardooffside@gmail.com>
2021-08-09 23:02:20 -0300

    create - PaginaCadastro

--- atividade06/src/components/pages/PaginaCadastro/index.js
+++ atividade06/src/components/pages/PaginaCadastro/index.js
@@ -1,9 +1,39 @@
+import { Form, Input, Button } from 'antd';
+import history from '../../History'
+import './index.css'
 
+function PaginaCadastro() {
 
-function PaginaCadastro(){
-    return(
-        <div>
-            <span>pagina de cadastro</span>
+    function BackFeed() {
+        history.push("/")
+    }
+
+    return (
+        <div className="containerForm">
+            <div className="formCadastro">
+                <span className="title">Cadastrar Usuário</span>
+                <Form className="formularioCadastro">
+                    <Form.Item label="Nome:" name="nome">
+                        <Input placeholder="escreva seu nome" />
+                    </Form.Item>
+
+                    <Form.Item label="E-mail:" name="email">
+                        <Input placeholder="escreva seu email" />
+                    </Form.Item>
+
+                    <Form.Item label="Senha:" name="senha">
+                        <Input placeholder="escreva sua senha" />
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{ offset: -1, span: 16 }}>
+
+                        <Button type="primary" htmlType="submit" onClick={BackFeed}>
+                            Submeter
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     )
 }
