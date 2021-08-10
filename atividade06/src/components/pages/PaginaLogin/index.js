8e3871bc13ccae80fb801a3074de0df6dbf0eb04
dududueedu <eduardooffside@gmail.com>
2021-08-09 23:17:20 -0300

    create - PaginaLogin

--- atividade06/src/components/pages/PaginaLogin/index.js
+++ atividade06/src/components/pages/PaginaLogin/index.js
@@ -1,9 +1,35 @@
-
+import { Form, Input, Button } from 'antd';
+import history from '../../History'
+import './index.css'
 
 function PaginaLogin(){
+    
+    function BackFeed() {
+        history.push("/")
+    }
+
     return(
-        <div>
-            <span>pagina de login</span>
+        <div className="containerForm">
+            <div className="formLogin">
+                <span className="title">Login</span>
+                <Form className="formularioLogin">
+                    <Form.Item label="" name="email">
+                        E-mail:<Input placeholder="escreva seu email" />
+                    </Form.Item>
+
+                    <Form.Item label="" name="senha">
+                        Senha:<Input.Password placeholder="escreva sua senha" />
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{ offset: -1, span: 16 }}>
+
+                        <Button type="primary" htmlType="submit" onClick={BackFeed}>
+                            Submeter
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     )
 }
